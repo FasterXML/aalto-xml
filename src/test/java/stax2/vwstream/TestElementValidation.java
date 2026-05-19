4a568d498e029d96651cfa78e2309df0a7903f10
Tatu Saloranta <tatu.saloranta@iki.fi>
2026-05-18 19:35:02 -0700

    ...

--- src/test/java/stax2/vwstream/TestElementValidation.java
+++ src/test/java/stax2/vwstream/TestElementValidation.java
@@ -134,7 +134,7 @@ public class TestElementValidation
             fail(modeDesc + " Expected a validation exception when closing element with missing required child");
         } catch (XMLValidationException vex) {
             // 18-May-2026, tatu: Extra space in there until Woodstox 7.2, so:
-            verifyException(vex, "element </root>: Expected element");
+            verifyException(vex, "element </root>: Expected");
             verifyException(vex, "<child>");
         }
     }
